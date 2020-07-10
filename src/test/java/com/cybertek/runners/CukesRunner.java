a40f46175438717d526687346051eae91dc1a291
gurhank <gurhankocyigit@gmail.com>
2020-07-10 15:36:01 -0500

    day 14 cucumber

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions"
 )
