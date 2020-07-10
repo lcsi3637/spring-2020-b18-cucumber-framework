bccbb7f814cae20196addfe557b1522d7f24e8c4
gurhank <gurhankocyigit@gmail.com>
2020-07-10 15:32:33 -0500

    day 14 cucumber

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -1,19 +1,30 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class GoogleStepDefinitions {
 
 
     @Given("User is on the google search page")
     public void user_is_on_the_google_search_page() {
-        System.out.println("USER IS ON THE GOOGLE SEARCH PAGE");
+
+        Driver.getDriver().get("https://www.google.com");
+
     }
 
 
     @Then("User should see title is Google")
     public void user_should_see_title_is_google() {
-        System.out.println("USER SEES THE TITLE IS GOOGLE");
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+        //Both assertions is not needed. Just for the example's sake.
+        Assert.assertTrue("Actual title does not match expected title!",actualTitle.equals(expectedTitle));
+        Assert.assertEquals("Actual title does not match expected title!", actualTitle, expectedTitle);
+
     }
 }
