383425176ebe82eb6f04ba5763ed01da1d77196b
gurhank <gurhankocyigit@gmail.com>
2020-07-10 16:06:54 -0500

    day 14 cucumber

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -12,6 +12,7 @@ public class GoogleStepDefinitions {
     public void user_is_on_the_google_search_page() {
         //Assert.fail("Unpurpose failing here!!!!!!!!");
 
+        //opening browser -> getting google.com
         Driver.getDriver().get("https://www.google.com");
 
 
@@ -20,11 +21,12 @@ public class GoogleStepDefinitions {
 
     @Then("User should see title is Google")
     public void user_should_see_title_is_google() {
-
+        //comparing actual title vs expected
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "Google";
 
-
+        //THIS LINE WILL FAIL THIS TEST NO MATTER WHAT
+        //Assert.fail("MY CUSTOM MESSAGE FROM MY ASSERT FAIL!..");
 
         //Both assertions is not needed. Just for the example's sake.
         Assert.assertTrue("Actual title does not match expected title!",actualTitle.equals(expectedTitle));
