1e245b8a5c15b8a9c9e9219d168004e6e239f857
gurhank <gurhankocyigit@gmail.com>
2020-07-13 11:58:57 -0500

    cucumber day2

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
         dryRun = false,
-        tags = "@employee and @sunday and not @librarian"
+        tags = "@loginWithBackground"
 )
 public class CukesRunner {
 }
