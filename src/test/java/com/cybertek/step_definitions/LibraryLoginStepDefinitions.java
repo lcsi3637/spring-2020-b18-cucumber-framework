4ac2f90d7aa9a2e31ce0bbd0f9441b167072b0a1
gurhank <gurhankocyigit@gmail.com>
2020-07-13 10:42:01 -0500

    cucumber day2

--- src/test/java/com/cybertek/step_definitions/LibraryLoginStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/LibraryLoginStepDefinitions.java
@@ -8,16 +8,16 @@ public class LibraryLoginStepDefinitions {
 
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
-
+        System.out.println("->User is on the login page");
     }
 
     @When("User logs in as librarian")
     public void user_logs_in_as_librarian() {
-
+        System.out.println("->User logs in as librarian");
     }
     @Then("User should see dashboard")
     public void user_should_see_dashboard() {
-        
+        System.out.println("->User should see dashboard");
     }
 
 }
